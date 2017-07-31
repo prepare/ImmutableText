09eb98484a42a3f67cf9c4684ed88cbac5dbbdb3
Mike Krüger <mikkrg@microsoft.com>
2017-07-31 07:52:05 +0200

    Corrected CSharp test.

--- csharp/ImmutableText/Program.cs
+++ csharp/ImmutableText/Program.cs
@@ -6,15 +6,15 @@ namespace ImmutableText
     {
         public static void Main(string[] args)
         {
-            for (int j = 0; j < 1000; j++)
+            for (int j = 0; j < 100; j++)
             {
                 var myText = new ImmutableText("hello");
-                for (int i = 0; i < 1000; i++)
+                for (int i = 0; i < 100000; i++)
                 {
                     myText = myText.InsertText(i, "1");
                 }
 
-                for (int i = 0; i < 1000; i++)
+                for (int i = 0; i < 100000; i++)
                 {
                     myText = myText.RemoveText(0, 1);
                 }
